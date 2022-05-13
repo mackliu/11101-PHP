adac7f62dec64e2a72198b211949c2878f803d63
mackliu <mackliu@gmail.com>
2022-05-13 13:19:55 +0800

    在computing.php中增加對GET及POST的判斷

--- form/computing.php
+++ form/computing.php
@@ -1,8 +1,17 @@
 <?php
+echo "POST陣列的內容";
+print_r($_POST);
+echo "<BR>";
+echo "get陣列的內容";
+print_r($_GET);
 
-
-$height=$_POST['height'];
-$weight=$_POST['weight'];
+if(empty($_GET)){ //判斷$_GET是否為空,如果為空表示為POST的方式傳送
+    $height=$_POST['height'];
+    $weight=$_POST['weight'];
+}else{
+    $height=$_GET['height'];
+    $weight=$_GET['weight'];
+};
 
 $bmi=round($weight/(($height/100)*($height/100)),1);
 
