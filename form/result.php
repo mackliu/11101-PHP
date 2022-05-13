77eed846b4634544e0da06f364fae2908deeb2c7
mackliu <mackliu@gmail.com>
2022-05-13 11:39:50 +0800

    在結果頁判斷BMI結果文字

--- form/result.php
+++ form/result.php
@@ -17,6 +17,17 @@
 
 <!--請根據BMI值,在畫面上秀出中文的BMI結果或健-->
 
+    <?php
+    if($bmi >= 27 ){
+        echo "肥胖";
+    }else if($bmi >= 24 && $bmi < 27){
+        echo "過重";
+    }else if($bmi >= 18.5 && $bmi < 24){
+        echo "健康";
+    }else if($bmi < 18.5){
+        echo "過輕";
+    }
+    ?>
 
 <div style="text-align:center">
 
