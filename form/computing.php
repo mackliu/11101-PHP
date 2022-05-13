f2942ae8ad2b46d64dd789e4de83d72cc8372421
mackliu <mackliu@gmail.com>
2022-05-13 11:48:56 +0800

    使用PHP簡寫法來顯示變數內容

--- form/computing.php
+++ form/computing.php
@@ -6,5 +6,19 @@ $weight=$_GET['weight'];
 
 $bmi=round($weight/(($height/100)*($height/100)),1);
 
-header("location:result.php?bmi=$bmi");
+$result='';
+
+if($bmi >= 27 ){
+    $result= "肥胖";
+}else if($bmi >= 24 && $bmi < 27){
+    $result= "過重";
+}else if($bmi >= 18.5 && $bmi < 24){
+    $result= "健康";
+}else if($bmi < 18.5){
+    $result= "過輕";
+}
+
+
+
+header("location:result.php?bmi=$bmi&result=$result");
 ?>
--- form/result.php
+++ form/result.php
@@ -8,26 +8,15 @@
 </head>
 <body>
 <h1 style="font-size:3rem;text-align:center">
-<?php
+你的BMI值為:<?=$_GET['bmi'];?>
 
-    $bmi=$_GET['bmi'];
-    echo "你的BMI為:".$bmi;
+</h1>
+<h2 style="text-align:center">判定結果為:<?=$_GET['result'];?></h2>
 
-?>
 
 <!--請根據BMI值,在畫面上秀出中文的BMI結果或健-->
 
-    <?php
-    if($bmi >= 27 ){
-        echo "肥胖";
-    }else if($bmi >= 24 && $bmi < 27){
-        echo "過重";
-    }else if($bmi >= 18.5 && $bmi < 24){
-        echo "健康";
-    }else if($bmi < 18.5){
-        echo "過輕";
-    }
-    ?>
+
 
 <div style="text-align:center">
 
