5226eda3a491f7e4632fac88b4a2b148c34ed6e2
mackliu <mackliu@gmail.com>
2022-05-16 11:57:01 +0800

    一個範例比較cooki和session

---
+++ cookie.php
@@ -0,0 +1,7 @@
+<h1>cookie</h1>
+<?php
+
+setcookie("name",'myname',time()+3600);
+
+echo $_COOKIE['name'];
+?>
---
+++ session.php
@@ -0,0 +1,8 @@
+<h1>session</h1>
+<?php
+session_start();
+$_SESSION['name']="myname";
+
+echo $_SESSION['name'];
+
+?>
