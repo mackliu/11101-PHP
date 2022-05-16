4ae3f52923e7a12d23e5bdfa7a9fc27c6000d381
mackliu <mackliu@gmail.com>
2022-05-16 09:46:49 +0800

    使用cookie來製作登入頁面

---
+++ session/chklogin.php
@@ -0,0 +1,19 @@
+<?php
+
+$default_user='mack';
+$default_pw='1234';
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$error='';
+if($acc!=$default_user || $pw!=$default_pw){
+    $error="帳號或密碼錯誤";
+    header("location:login.php?error=$error");
+}else{
+    setcookie('login',$acc,time()+360);
+    header("location:memcenter.php");
+
+}
+
+?>
---
+++ session/login.php
@@ -0,0 +1,43 @@
+<?php
+if(isset($_COOKIE['login'])){
+    header("location:memcenter.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員登入</title>
+</head>
+<body>
+<h1>會員登入</h1>
+<?php
+
+if(!empty($_GET['error'])){
+    echo "<h3 style='color:red'>{$_GET['error']}</h3>";
+}
+
+?>
+<form action="chklogin.php" method="post">
+    <table>
+        <tr>
+            <td>帳號:</td>
+            <td><input type="text" name="acc" id=""></td>
+        </tr>
+        <tr>
+            <td>密碼:</td>
+            <td><input type="password" name="pw" id=""></td>
+        </tr>
+    </table>
+    <div>
+        <input type="submit" value="登入">
+        <input type="reset" value="重置">
+    </div>
+</form>
+
+</body>
+</html>
